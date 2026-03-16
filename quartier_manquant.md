41b1854ecfffe2b72c843a65a37d8ed712719327
Dvxy67 <jean.ardaillou1@gmail.com>
2026-03-16 15:38:00 +0100

    quartier manquant

---
+++ quartier_manquant.md
@@ -0,0 +1,10 @@
+**liste des quartier manquant**
+Shakespeare
+Albert 1er
+Craps
+Orphelinat
+Asters
+Sillon
+Democratie
+Gryzon
+La Digue
--- test-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$link = mysqli_connect('db.ardjea.dreamhosters.com', 'jean_ardaillou', '201657lachaiseVide1*', 'foyer_wp');
-
-if (!$link) {
-    die('Erreur de connexion : ' . mysqli_connect_error());
-}
-echo 'Connexion réussie à la base de données !';
-mysqli_close($link);
-?>
